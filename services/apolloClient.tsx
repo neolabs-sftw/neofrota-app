ac05b5bcfbc24d4c89c1478bed324dc4e6c0c428
Neolabs-sftw <neolabs.sftw@gmail.com>
2026-04-08 21:43:42 -0300

    Feat: ajuste em toda parte do financeiro do motorista

--- services/apolloClient.tsx
+++ services/apolloClient.tsx
@@ -2,9 +2,9 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    // uri: "https://neofrota-api.vercel.app/graphql",
+    uri: "https://neofrota-api.vercel.app/graphql",
     // uri: "https://neofrotaapi.vercel.app/graphql",
-    uri: "http://192.168.1.3:4000/graphql",
+    // uri: "http://192.168.1.3:4000/graphql",
     // uri: "http://172.20.10.3:4000/graphql",
     // uri: "http://192.168.100.78:4000/graphql",
   }),
